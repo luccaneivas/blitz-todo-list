5d04f765078da2a16c0d81da987daf2e7dc4ae6c
luccaneivas <luccaneiva12@gmail.com>
2022-07-03 22:04:25 -0300

    rebuild: separa server e app

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
-const express = require('express');
+const app = require('./app');
 
 const PORT = process.env.PORT;
-
-const app = express();
-app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on port ${3000}`));
