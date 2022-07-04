2b02cb7b7e48e58c88a816ec94dd63e27093dee3
luccaneivas <luccaneiva12@gmail.com>
2022-07-04 01:35:36 -0300

    feat: cria rota list

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+const listRoute = require('./routes/list');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/list', listRoute);
 
 module.exports = app;
